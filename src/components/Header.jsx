d3cd606ee134034f8cbb8bcd1a02a006e5cf0a43
YUVRAJSINGH3112 <yuvrajsingh3112s@gmail.com>
2025-02-21 02:34:13 +0530

    nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 const Header = () => {
   return (
     <nav className="navbar">
-        <img src="/rm.jpg" alt="" className="logo" />
+        <img src="" alt=""  className="logo" />
         <div className="nav-btn">
         <a href="">Home</a>
         <a href="">Docs</a>
